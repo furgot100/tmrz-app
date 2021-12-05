1b192113656ba013999409ee4ae5e7b912417ef5
Francis Tsang <frtsang40@gmail.com>
2021-12-04 18:43:07 -0800

    added list of timers and styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import reducers from './reducers';
 
+import NewTimer from './components/NewTimer'
+import ListTimers from './components/ListTimers'
+
 const store = createStore(reducers);
 
 function App() {
   return (
     <Provider store={store}>
-      <h1>TMRZ</h1>
+      <div className="App">
+        <h1>TMRZ</h1>
+        <NewTimer />
+        <ListTimers />
+      </div>
     </Provider>
   );
 }
